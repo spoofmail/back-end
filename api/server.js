baa8f1bbebe776bcd47f9b1058fa11b74ad84591
jackcornellober <jackcornellober@gmail.com>
2019-07-29 16:50:48 -0700

    fixed add helper

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+  res.send("Welcome to the Spoofmail Backend!");
 });
 
 module.exports = server;
